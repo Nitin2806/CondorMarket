1ac0a78e3d267d50e637001c9b3f0a51f9e74c01
Vanamuthu <95688625+Vanamuthu@users.noreply.github.com>
2024-08-13 00:04:19 -0400

    Sorting

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import './CSS/ShopCategory.css';
 import { ShopContext } from '../Context/ShopContext';
-import dropdown_icon from '../components/Assets/dropdown_icon.png';
+
 import Item from '../components/Item/Item';
 
 const ShopCategory = (props) => {
@@ -33,13 +33,13 @@ const ShopCategory = (props) => {
           <span>Showing 1-{Math.min(visibleProducts, filteredProducts.length)}</span> out of {filteredProducts.length} products
         </p>
         <div className="shopcategory-sort">
-          Sort by 
+          Sort by   
           <select onChange={(e) => setSortOption(e.target.value)} value={sortOption}>
             <option value="default">Default</option>
             <option value="priceLowToHigh">Price: Low to High</option>
             <option value="priceHighToLow">Price: High to Low</option>
           </select>
-          <img src={dropdown_icon} alt="dropdown_icon" />
+          
         </div>
       </div>
       <div className="shopcategory-products">
